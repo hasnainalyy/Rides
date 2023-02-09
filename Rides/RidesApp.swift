6d3fe96596cdde6270e61be239273d2437682f1b
Hasnain <hasnainmak@gmail.com>
2023-02-09 11:53:56 -0400

    Initial Commit

---
+++ Rides/RidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RidesApp.swift
+//  Rides
+//
+//  Created by Ali Hussain on 2023-02-09.
+//
+
+import SwiftUI
+
+@main
+struct RidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
